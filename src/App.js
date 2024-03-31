e18566fccd0eddfe7a2e6f5aa308b7f58ca41473
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2024-03-31 17:28:28 +0430

    Update src/App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Team from './components/Team';
+import Service from './components/Service';
+import Plan from './components/Plan';
+
 function App() {
   return (
     <>
-      App
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/our-team" element={<Team />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/plan" element={<Plan />} />
+        </Routes>
+      </Router>
     </>
   );
 }
-
 export default App;
